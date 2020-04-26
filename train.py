47037b202d743af07e4477102c542b4fb0ff069e
Marshelix <martin.sanner@gmx.de>
2020-04-27 00:37:47 +0200

    Added test data loading and prediction generation

--- train.py
+++ train.py
@@ -27,6 +27,9 @@ from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 
+#Adam stuff
+import pandas as pd
+
 '''
     File to train a simple cnn on image data gathered with pi-car
     Model built and trained using keras, transferrable to tensorflow lite
@@ -37,6 +40,47 @@ model_output_dir = "./models/"
 mode = "**"
 
 results = {}
+
+def load_test_dataset(resize_percent = 60):
+    dirname = "./test_data/test_data/**.png"
+    fnames_full = glob.glob(dirname, recursive = True)
+    print(str(len(fnames_full)) + " names found.")
+    images = [[] for i in range(len(fnames_full))]  #override image according to name
+    for fname in fnames_full:
+        img = pltimg.imread(fname)
+        width = int(img.shape[1] * resize_percent / 100)
+        height = int(img.shape[0] * resize_percent / 100)
+        dim = (width, height)
+        resized = cv2.resize(img,dim,interpolation = cv2.INTER_AREA)
+        name_idx = int(fname.split("\\")[1].split(".")[0])-1
+        images[name_idx] = resized
+    return np.asarray([images])
+
+def load_adam_dataset(resize_percent = 60):
+    train_df = pd.read_csv("training_norm.csv")
+    train_df.index = train_df["image_id"]
+    angles = train_df.angle
+    speed = train_df.speed
+    targets = []
+    images = []
+    dirname = "./training_data/**/*.png"
+    print("Loading data from "+dirname)
+    fnames_full = glob.glob(dirname,recursive = True)
+    print(str(len(fnames_full)) + " names found")
+    for fname in fnames_full:
+        fname_split = fname.split("\\")
+        #print("Loading {}".format(fname))
+        name = fname_split[2]
+        img = pltimg.imread(fname)
+        width = int(img.shape[1] * resize_percent / 100)
+        height = int(img.shape[0] * resize_percent / 100)
+        dim = (width, height)
+        resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+        images.append(resized)
+        targets.append([float(train_df.angle[int(name.split(".")[0])]),float(train_df.speed[int(name.split(".")[0])])])
+    return images,targets
+
+adam_images,adam_targets = load_adam_dataset()
 
 def load_dataset(resize_percent =  60):
     images = []
@@ -49,8 +93,9 @@ def load_dataset(resize_percent =  60):
         fname_split = fname.split("\\")
         
         name = fname_split[2]
-        speed = name.split(".")[0].split("_")[2]
-        angle = name.split(".")[0].split("_")[1]
+        #print("Loading {}".format(fname))
+        speed = (float(name.split(".")[0].split("_")[2]))/35.0
+        angle = (float(name.split(".")[0].split("_")[1])-50.0)/80.0
         img = pltimg.imread(fname)
         width = int(img.shape[1] * resize_percent / 100)
         height = int(img.shape[0] * resize_percent / 100)
@@ -63,9 +108,9 @@ def load_dataset(resize_percent =  60):
     return images, targets
 
 
-
-
 imgs, targets = load_dataset()
+imgs = imgs + adam_images
+targets = targets + adam_targets
 in_shape = imgs[0].shape
 X_train, X_valid, y_train, y_valid = train_test_split( imgs, targets, test_size=0.2)
 print("Training data: %d\nValidation data: %d" % (len(X_train), len(X_valid)))
@@ -86,7 +131,7 @@ def Model():
     model.add(Dense(20, activation = "elu"))
     model.add(Dense(2,activation = "elu"))
     
-    optimizer = Adam(lr = 1e-2)
+    optimizer = Adam(lr = 1e-3)
     model.compile(loss = "mse", optimizer = optimizer)
     return model
 
@@ -110,26 +155,42 @@ def image_data_gen(imgs, targets,batch_size):
 
         
 checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(filepath=os.path.join(model_output_dir,'AI_driver_check_all.h5'), verbose=1, save_best_only=True)
-    
+es_callback = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0, patience=10, verbose=1, mode='auto', baseline=None, restore_best_weights=True)
 history = model.fit_generator(image_data_gen( X_train, y_train, batch_size=100),
                               steps_per_epoch=300,
-                              epochs=50,
+                              epochs=500,
                               validation_data = image_data_gen( X_valid, y_valid, batch_size=100),
                               validation_steps=200,
                               verbose=1,
                               shuffle=1,
-                              callbacks=[checkpoint_callback])
+                              callbacks=[checkpoint_callback,es_callback])
 # always save model output as soon as model finishes training
 name = os.path.join(model_output_dir,'AI_driver_all.h5')
 model.save(name)
 plt.figure()
 plt.plot(history.history['loss'],color='blue')
+plt.legend(["training loss"])
+plt.figure()
 plt.plot(history.history['val_loss'],color='red')
-plt.legend(["training loss", "validation loss"])
+plt.legend([ "validation loss"])
 results[mode] = [history.history['loss'][-1],history.history['val_loss'][-1]]
 
-converter = tf.lite.TFLiteConverter.from_keras_model(model)
-tflite_model = converter.convert()
-conv_name = os.path.join(model_output_dir,"converted_model.tflite")
-open(conv_name, "wb").write(tflite_model)
+
+#create prediction
+colnames = ["angle","speed"]
+test_images = load_test_dataset()
+prediction = model.predict(test_images[0])
+df = pd.DataFrame(prediction, columns = colnames)
+df["image_id"] = range(1,len(test_images)+1)
+df = df.reindex(columns = ["image_id","angle","speed"])
+df.index = df["image_id"]
+df = df[["angle","speed"]]
+#write
+df.to_csv("prediction.csv")
+
+
+#converter = tf.lite.TFLiteConverter.from_keras_model(model)
+#tflite_model = converter.convert()
+#conv_name = os.path.join(model_output_dir,"converted_model.tflite")
+#open(conv_name, "wb").write(tflite_model)
 
